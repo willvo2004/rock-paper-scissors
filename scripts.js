f0baf980ac6d05416b74193d28ad557e8ac586a4
willvo2004 <willvo2004@gmail.com>
2024-01-08 21:00:56 -0500

    Added score tracker feature

--- scripts.js
+++ scripts.js
@@ -3,20 +3,27 @@ function getComputerChoice() {
     return arr[Math.floor(Math.random() * 3)];
 }
 
+let playerCount = 0;
+let computerCount = 0;
+
 function playRound(playerSelection, computerSelection) {
     const playerWin = (playerSelection == "rock" && computerSelection == "scissors") ? true : (playerSelection == "paper" && computerSelection == "rock") ? true : (playerSelection == "scissors" && computerSelection == "paper") ? true : false;
     const tie = playerSelection == computerSelection;
     const computerWin = !playerWin && !tie;
 
-    const playerCount = 0;
-    const computerCount = 0;
+    
+    const playerScore = document.querySelector(".human-score-value");
+    const computerScore = document.querySelector(".computer-score-value");
 
     if (playerWin) {
-       // document.querySelector("#human-score").textContent = "You: " + playerCount;
+        //document.querySelector(".human-score").textContent = "You: " + playerCount;
+        playerCount++;
+        playerScore.innerHTML = playerCount;
         return "You Win! " + playerSelection + " beats "  + computerSelection;
     }
     else if (computerWin) {
-       // document.querySelector("#computer-score").textContent = "CPU: " + computerCount;
+        computerCount++;
+        computerScore.innerHTML = computerCount;
         return "You Lose! "  + computerSelection + " beats " + playerSelection;
     }
     else {
@@ -43,6 +50,8 @@ scissors.addEventListener("click", () => {
     const result = playRound("scissors", getComputerChoice());
     header.textContent = result;
 });
+
+
 
 function game() {
     for (let i = 0; i < 5; i++) {
